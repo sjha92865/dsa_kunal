c6403687c62af49abe891393cdeab5e079143b71
Shubham Jha <71147870+sjha92865@users.noreply.github.com>
2024-08-04 08:37:50 +0530

    Create subarray.py

---
+++ subarray.py
@@ -0,0 +1,5 @@
+# subarray is not taught by kunal
+# subset, subsequence is same, both can elements like (a,b,c)--> ac, maintains order.
+# but subarray should have continuos & same order elements only(bc,ab).
+# check subarray using recursion and without recursion.
+
